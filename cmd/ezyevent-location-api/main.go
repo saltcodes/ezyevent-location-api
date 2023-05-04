5de645565fcf6f9deda5af951883e0d1c6d4bd06
Johnson Osei Yeboah <johnyoat@gmail.com>
2023-05-04 01:48:24 -0500

    chore:removed fiber rest endpoints

--- cmd/ezyevent-location-api/main.go
+++ cmd/ezyevent-location-api/main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"ezyevent-location-api/internal/handler"
 	"ezyevent-location-api/internal/proto"
 	route "ezyevent-location-api/internal/route_guide"
-	"github.com/gofiber/fiber/v2"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 )
 
 func main() {
-
-	//Initializing Fiber
-	app := fiber.New()
-	handler.InitRoutes(app)
-	go func() {
-		log.Fatalln(app.Listen(":8081"))
-	}()
 
 	//gRPC Connection establishing using tcp
 	con, err := net.Listen("tcp", ":8181")
@@ -34,5 +25,4 @@ func main() {
 	if rpcError != nil {
 		log.Fatal(rpcError)
 	}
-
 }
