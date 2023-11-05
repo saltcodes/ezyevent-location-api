4858892d6ee6edae03900fb9c5acad959d601050
Johnson Osei Yeboah <johnyoat@gmail.com>
2023-11-05 01:08:36 -0600

    feat:  insert or update for event location data

--- internal/route_guide/location_service.go
+++ internal/route_guide/location_service.go
@@ -2,6 +2,8 @@ package route_guide
 
 import (
 	"ezyevent-location-api/internal/proto"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/net/context"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
@@ -9,6 +11,16 @@ import (
 var eventsCollection = db.Collection("events-location")
 
 func (s *Server) LocationData(ctx context.Context, data *proto.LocationObject) (*emptypb.Empty, error) {
-	_, err := eventsCollection.InsertOne(context.TODO(), data)
+	//filter for checking Id
+	filter := bson.D{{"id", data.Id}}
+
+	//Data prep
+	d := bson.D{{"$set", data}}
+
+	//Config for save or update
+	opts := options.Update().SetUpsert(true)
+
+	//Actual executions
+	_, err := eventsCollection.UpdateOne(context.TODO(), filter, d, opts)
 	return new(emptypb.Empty), err
 }
